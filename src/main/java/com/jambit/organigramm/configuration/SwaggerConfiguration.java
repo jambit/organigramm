832a555b407422c96756fae58d882161153e2f57
Alexander Neuhäuser <alexander.neuhaeuser.py@gmail.com>
2020-02-17 18:29:34 +0100

    add module and basic endpoint

--- src/main/java/com/jambit/organigramm/configuration/SwaggerConfiguration.java
+++ src/main/java/com/jambit/organigramm/configuration/SwaggerConfiguration.java
@@ -1,4 +1,4 @@
-package com.jambit.organigramm.config;
+package com.jambit.organigramm.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
